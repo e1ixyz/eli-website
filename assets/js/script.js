d1785501cad886955c8986bb52d7e40294a3f602
Eli McGehee <elimcgehee2001@gmail.com>
2023-02-04 14:39:46 -0500

    preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+// Join Alert
 var alerted = localStorage.getItem('alerted') || '';
 if (alerted != 'yes') {
  alert("Welcome! For the best user experience, please enable audio in your browser settings.");
  localStorage.setItem('alerted','yes');
   }
 
+// Audio Button
 function playMyAudio(){
     document.getElementById("myAudio").play();
   }
+
+// Preloader
+document.addEventListener("DOMContentLoaded", function() {
+  const preloader = document.getElementById("preloader");
+  const mainContent = document.getElementById("main-content");
+  setTimeout(function() {
+    preloader.style.display = "none";
+    mainContent.classList.add("visible");
+  }, 500);
+});
