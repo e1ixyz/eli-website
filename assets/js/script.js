3e7dc858c3e812805aa684ce28e4939c01ea1ae9
Eli McGehee <elimcgehee2001@gmail.com>
2023-05-03 16:48:26 -0400

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,27 +19,3 @@ document.addEventListener("DOMContentLoaded", function() {
     mainContent.classList.add("visible");
   }, 500);
 });
-
-
-// Gradient
-/* function generateGradient() {
-  var colorValues = ["#1e90ff", "#9370db", "#ff69b4", "#c0c0c0", "#ffffff", "#ffa07a"];
-
-  function generateRandomColor() {
-    var x = Math.floor(Math.random() * colorValues.length);
-    return colorValues[x];
-  }
-
-  var newColor1 = generateRandomColor();
-  var newColor2 = generateRandomColor();
-  var angle = Math.round( Math.random() * 360 );
-
-  var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ")";
-
-  var cardHeader = document.querySelector(".card-header");
-  cardHeader.style.background = gradient;
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  generateGradient();
-}); */
