70f320668ef83e035bebc8789bea198e3853d18e
e1ixyz <elimcgehee2001@gmail.com>
2023-11-29 19:13:55 -0500

    glitch button

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,56 @@ document.addEventListener("DOMContentLoaded", function() {
     mainContent.classList.add("visible");
   }, 500);
 });
+
+// Random Color Glitch
+
+let colorizeEnabled = false;
+let originalColors = {};
+
+function toggleColorize() {
+    if (!colorizeEnabled) {
+        colorizeEnabled = true;
+        randomizeColors();
+    } else {
+        colorizeEnabled = false;
+        revertColors();
+    }
+}
+
+function randomizeColors() {
+    const elements = document.querySelectorAll('body *');
+
+    elements.forEach(element => {
+        originalColors[element] = {
+            color: element.style.color,
+            backgroundColor: element.style.backgroundColor
+        };
+
+        const randomColor = getRandomColor();
+        element.style.color = randomColor;
+        element.style.backgroundColor = getRandomColor();
+    });
+}
+
+function revertColors() {
+    const elements = document.querySelectorAll('body *');
+
+    elements.forEach(element => {
+        const originalColor = originalColors[element];
+        if (originalColor) {
+            element.style.color = originalColor.color;
+            element.style.backgroundColor = originalColor.backgroundColor;
+        }
+    });
+
+    originalColors = {};
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
