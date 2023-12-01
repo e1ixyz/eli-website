5ebaaae510c5d6412ec36665c630845e80033ead
e1ixyz <elimcgehee2001@gmail.com>
2023-12-01 18:01:29 -0500

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,3 +72,65 @@ function getRandomColor() {
     }
     return color;
 }
+
+// Dark Mode
+
+var isToggled = false; // Define isToggled variable here
+        
+function setBackgroundImage(image) {
+    document.body.style.backgroundImage = "url('assets/img/" + image + "')";
+}
+
+var bgImages = ["bg1.gif", "bg2.gif", "bg3.gif"];
+var currentBgIndex = getRandomIndex(bgImages); // Randomly select an initial background
+
+setBackgroundImage(bgImages[currentBgIndex]);
+
+function toggleStylesheet() {
+    isToggled = !isToggled;
+
+    if (isToggled) {
+        document.getElementById("stylesheet").setAttribute("href", "assets/css/win95_dark.css");
+        setBackgroundImage("bg_dark.gif");
+    } else {
+        document.getElementById("stylesheet").setAttribute("href", "assets/css/win95.css");
+        // Reset the currentBgIndex to choose a random background from the array
+        currentBgIndex = getRandomIndex(bgImages);
+        setBackgroundImage(bgImages[currentBgIndex]);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add a click event listener to the button
+    document.getElementById("toggleButton").addEventListener("click", toggleStylesheet);
+});
+
+function cycleBackgroundImages() {
+    currentBgIndex = (currentBgIndex + 1) % bgImages.length;
+    var randomBg = bgImages[currentBgIndex];
+    setBackgroundImage(randomBg);
+}
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+// Client Portal Redirect
+
+document.addEventListener("DOMContentLoaded", function () {
+    $('#redirectToAdmin').click(function () {
+        var inputDomain = $('#inputDomain').val();
+        if (inputDomain !== "") {
+            // Check if the input contains 'http://' or 'https://', if not, add 'https://'
+            if (!inputDomain.match(/^https?:\/\//i)) {
+                inputDomain = 'https://' + inputDomain;
+            }
+            // Append '/admin' to the input domain
+            var adminURL = inputDomain + '/admin';
+            // Redirect the user to the admin URL
+            window.location.href = adminURL;
+        } else {
+            alert("Please enter a valid website domain.");
+        }
+    });
+});
